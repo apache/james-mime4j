3a78b0720d12a25fd2a328b169f136fcd66c24df
Niklas Therning <niklas@apache.org>
2008-07-21 10:15:41 +0000

    Saved txt files will now use the mime charset specified for the body part instead of the Java default.
    git-svn-id: https://svn.apache.org/repos/asf/james/mime4j/trunk@678382 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/james/mime4j/message/MessageParserTest.java
+++ src/test/java/org/apache/james/mime4j/message/MessageParserTest.java
@@ -227,8 +227,13 @@ public class MessageParserTest extends TestCase {
     private void writeToFile(Body b, File mime4jFile)
             throws FileNotFoundException, IOException {
         if (b instanceof TextBody) {
+            String charset = CharsetUtil.toJavaCharset(b.getParent().getCharset());
+            if (charset == null) {
+                charset = "ISO8859-1";
+            }
+
             OutputStream out = new FileOutputStream(mime4jFile);
-            IOUtils.copy(((TextBody) b).getReader(), out);
+            IOUtils.copy(((TextBody) b).getReader(), out, charset);
         } else {
             OutputStream out = new FileOutputStream(mime4jFile);
             IOUtils.copy(((BinaryBody) b).getInputStream(), out);
