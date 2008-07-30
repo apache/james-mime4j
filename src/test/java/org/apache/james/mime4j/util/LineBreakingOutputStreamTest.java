00c9d8f54eb02b81566493f647eaed970e41fa1b
Stefano Bagnara <bago@apache.org>
2008-07-30 11:31:07 +0000

    Add some sort of unit tests for LineBreakingOutputStream
    git-svn-id: https://svn.apache.org/repos/asf/james/mime4j/trunk@680984 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/james/mime4j/util/LineBreakingOutputStreamTest.java
+++ src/test/java/org/apache/james/mime4j/util/LineBreakingOutputStreamTest.java
@@ -19,16 +19,67 @@
 
 package org.apache.james.mime4j.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 import junit.framework.TestCase;
 
+/**
+ * Test for LineBreakingOutputStream
+ */
 public class LineBreakingOutputStreamTest extends TestCase {
     
     public void testLongLine() throws IOException {
-        OutputStream os = new LineBreakingOutputStream(System.out, 4);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        OutputStream os = new LineBreakingOutputStream(out, 4);
         os.write("gsdfklgsjdhflkgjhsdfklgjhsdlkfjghksdljfhgslkdjfhgklsjdhf".getBytes());
+        os.flush();
+        os.close();
+        String expected = "gsdf\r\nklgs\r\njdhf\r\nlkgj\r\nhsdf\r\nklgj\r\nhsdl\r\nkfjg\r\nhksd\r\nljfh\r\ngslk\r\ndjfh\r\ngkls\r\njdhf";
+        assertEquals(new String(out.toByteArray()), expected);
+    }
+    
+    public void testLineLengthEqualInput() throws IOException {
+        String input = "gsdfklgsjdhflkgjhsdfklgjhsdlkfjghksdljfhgslkdjfhgklsjdhf";
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        OutputStream os = new LineBreakingOutputStream(out, input.length());
+        os.write(input.getBytes());
+        os.flush();
+        os.close();
+        assertEquals(new String(out.toByteArray()), input);
+    }
+    
+    public void testLineLengthLongerThanInput() throws IOException {
+        String input = "gsdfklgsjdhflkgjhsdfklgjhsdlkfjghksdljfhgslkdjfhgklsjdhf";
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        OutputStream os = new LineBreakingOutputStream(out, input.length()+1);
+        os.write(input.getBytes());
+        os.flush();
+        os.close();
+        assertEquals(new String(out.toByteArray()), input);
+    }
+    
+    public void testLineLengthShorterThanInput() throws IOException {
+        String input = "gsdfklgsjdhflkgjhsdfklgjhsdlkfjghksdljfhgslkdjfhgklsjdhf";
+        String expected = "gsdfklgsjdhflkgjhsdfklgjhsdlkfjghksdljfhgslkdjfhgklsjdh\r\nf";
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        OutputStream os = new LineBreakingOutputStream(out, input.length()-1);
+        os.write(input.getBytes());
+        os.flush();
+        os.close();
+        assertEquals(new String(out.toByteArray()), expected);
+    }
+    
+    public void testZeroLengthInput() throws IOException {
+        String input = "";
+        String expected = "";
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        OutputStream os = new LineBreakingOutputStream(out, 10);
+        os.write(input.getBytes());
+        os.flush();
+        os.close();
+        assertEquals(new String(out.toByteArray()), expected);
     }
 
 }
