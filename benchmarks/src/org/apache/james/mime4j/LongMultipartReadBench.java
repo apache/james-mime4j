da99013500a43ca1dd502881d0ce38959f03ea08
Markus Wiederkehr <mwiederkehr@apache.org>
2008-12-29 19:23:47 +0000

    added ASF header; use of CodecUtil.copy(); changed unit to seconds
    git-svn-id: https://svn.apache.org/repos/asf/james/mime4j/trunk@729976 13f79535-47bb-0310-9956-ffa450edef68

--- benchmarks/src/org/apache/james/mime4j/LongMultipartReadBench.java
+++ benchmarks/src/org/apache/james/mime4j/LongMultipartReadBench.java
@@ -1,9 +1,29 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
 package org.apache.james.mime4j;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
+import org.apache.james.mime4j.decoder.CodecUtil;
 import org.apache.james.mime4j.parser.MimeTokenStream;
 
 public class LongMultipartReadBench {
@@ -19,11 +39,7 @@ public class LongMultipartReadBench {
             return;
         }
         try {
-            int l;
-            byte[] tmp = new byte[2048];
-            while ((l = instream.read(tmp)) != -1) {
-                outstream.write(tmp, 0, l);
-            }
+            CodecUtil.copy(instream, outstream);
         } finally {
             instream.close();
         }
@@ -51,7 +67,7 @@ public class LongMultipartReadBench {
         }
         long finish = System.currentTimeMillis();
         System.out.println("Execution time: " 
-                + ((double)(finish - start) / 1000) + " ms" );
+                + ((double)(finish - start) / 1000) + " s" );
     }
     
 }
