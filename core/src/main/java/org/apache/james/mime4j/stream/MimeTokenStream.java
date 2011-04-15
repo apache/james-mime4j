2570f156d6ce81d0e0481c92a7360914c687ca3a
Felix Knecht <felixk@apache.org>
2011-04-15 04:47:21 +0000

    Remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/james/mime4j/trunk@1092586 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/james/mime4j/stream/MimeTokenStream.java
+++ core/src/main/java/org/apache/james/mime4j/stream/MimeTokenStream.java
@@ -24,8 +24,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.Charset;
-import java.nio.charset.IllegalCharsetNameException;
-import java.nio.charset.UnsupportedCharsetException;
 import java.util.LinkedList;
 
 import org.apache.james.mime4j.MimeException;
