0c5c74bb7b46b698c045a942a08335732c935f60
Norman Maurer <norman@apache.org>
2011-06-17 09:03:09 +0000

    Just replace tabs with spaces
    git-svn-id: https://svn.apache.org/repos/asf/james/mime4j/trunk@1136813 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/james/mime4j/stream/MimeEntityConfig.java
+++ core/src/main/java/org/apache/james/mime4j/stream/MimeEntityConfig.java
@@ -34,8 +34,8 @@ public final class MimeEntityConfig implements Cloneable {
     private boolean countLineNumbers;
     private String headlessParsing;
     private boolean malformedHeaderStartsBody;
-    
-	public MimeEntityConfig() {
+
+    public MimeEntityConfig() {
         this.strictParsing = false;
         this.countLineNumbers = false;
         this.malformedHeaderStartsBody = false;
@@ -45,33 +45,35 @@ public final class MimeEntityConfig implements Cloneable {
         this.maxContentLen = -1;
         this.headlessParsing = null;
     }
-    
+
     /**
      * @see #setMalformedHeaderStartsBody(boolean)
      * 
-     * @return true if malformed header should "end" the headers and be 
-     * part of the body
+     * @return true if malformed header should "end" the headers and be part of
+     *         the body
      */
     public boolean isMalformedHeaderStartsBody() {
-		return malformedHeaderStartsBody;
-	}
+        return malformedHeaderStartsBody;
+    }
 
-	/**
-	 * Define the behaviour for dealing with malformed headers while in lenient
-	 * mode
-	 * 
-	 * @param malformedHeaderStartsBody <code>true</code> to make the parser
-	 *            interpret a malformed header as end of the headers and
-	 *            as part of the body (as if the CRLF separator was missing).
-	 *            <code>false</code> to simply ignore malformed headers and
-	 *            continue parsing headers from the following line.
-	 */
-	public void setMalformedHeaderStartsBody(boolean malformedHeaderStartsBody) {
-		this.malformedHeaderStartsBody = malformedHeaderStartsBody;
-	}
+    /**
+     * Define the behaviour for dealing with malformed headers while in lenient
+     * mode
+     * 
+     * @param malformedHeaderStartsBody
+     *            <code>true</code> to make the parser interpret a malformed
+     *            header as end of the headers and as part of the body (as if
+     *            the CRLF separator was missing). <code>false</code> to simply
+     *            ignore malformed headers and continue parsing headers from the
+     *            following line.
+     */
+    public void setMalformedHeaderStartsBody(boolean malformedHeaderStartsBody) {
+        this.malformedHeaderStartsBody = malformedHeaderStartsBody;
+    }
 
     /**
      * Returns the value of the strict parsing mode
+     * 
      * @see #setStrictParsing(boolean)
      * 
      * @return value of the strict parsing mode
@@ -81,22 +83,24 @@ public final class MimeEntityConfig implements Cloneable {
     }
 
     /**
-     * Defines whether minor violations of the MIME specification should be 
+     * Defines whether minor violations of the MIME specification should be
      * tolerated or should result in a {@link MimeException}. If this parameter
-     * is set to <code>true</code>, a strict interpretation of the MIME 
-     * specification will be enforced, If this parameter is set to <code>false</code>
-     * minor violations will result in a warning in the log.
+     * is set to <code>true</code>, a strict interpretation of the MIME
+     * specification will be enforced, If this parameter is set to
+     * <code>false</code> minor violations will result in a warning in the log.
      * <p>
      * Default value: <code>false</code>
      * 
-     * @param strictParsing value of the strict parsing mode
+     * @param strictParsing
+     *            value of the strict parsing mode
      */
     public void setStrictParsing(boolean strictParsing) {
         this.strictParsing = strictParsing;
     }
 
-    /** 
+    /**
      * Returns the maximum line length limit
+     * 
      * @see #setMaxLineLen(int)
      * 
      * @return value of the the maximum line length limit
@@ -106,21 +110,23 @@ public final class MimeEntityConfig implements Cloneable {
     }
 
     /**
-     * Sets the maximum line length limit. Parsing of a MIME entity will be terminated 
-     * with a {@link MimeException} if a line is encountered that exceeds the maximum
-     * length limit. If this parameter is set to a non positive value the line length
-     * check will be disabled.
+     * Sets the maximum line length limit. Parsing of a MIME entity will be
+     * terminated with a {@link MimeException} if a line is encountered that
+     * exceeds the maximum length limit. If this parameter is set to a non
+     * positive value the line length check will be disabled.
      * <p>
      * Default value: <code>1000</code>
      * 
-     * @param maxLineLen maximum line length limit
+     * @param maxLineLen
+     *            maximum line length limit
      */
     public void setMaxLineLen(int maxLineLen) {
         this.maxLineLen = maxLineLen;
     }
-    
-    /** 
+
+    /**
      * Returns the maximum header limit
+     * 
      * @see #setMaxHeaderCount(int)
      * 
      * @return value of the the maximum header limit
@@ -130,14 +136,15 @@ public final class MimeEntityConfig implements Cloneable {
     }
 
     /**
-     * Sets the maximum header limit. Parsing of a MIME entity will be terminated 
-     * with a {@link MimeException} if the number of headers exceeds the maximum
-     * limit. If this parameter is set to a non positive value the header limit check 
-     * will be disabled.
+     * Sets the maximum header limit. Parsing of a MIME entity will be
+     * terminated with a {@link MimeException} if the number of headers exceeds
+     * the maximum limit. If this parameter is set to a non positive value the
+     * header limit check will be disabled.
      * <p>
      * Default value: <code>1000</code>
      * 
-     * @param maxHeaderCount maximum header limit
+     * @param maxHeaderCount
+     *            maximum header limit
      */
     public void setMaxHeaderCount(int maxHeaderCount) {
         this.maxHeaderCount = maxHeaderCount;
@@ -145,6 +152,7 @@ public final class MimeEntityConfig implements Cloneable {
 
     /**
      * Returns the maximum header length limit
+     * 
      * @see #setMaxHeaderLen(int)
      * 
      * @return value of the maximum header length limit
@@ -154,25 +162,28 @@ public final class MimeEntityConfig implements Cloneable {
     }
 
     /**
-     * Sets the maximum header length limit. Parsing of a MIME entity will be terminated 
-     * with a {@link MimeException} if the total length of a header exceeds this limit.
-     * If this parameter is set to a non positive value the header length check will be
-     * disabled.
+     * Sets the maximum header length limit. Parsing of a MIME entity will be
+     * terminated with a {@link MimeException} if the total length of a header
+     * exceeds this limit. If this parameter is set to a non positive value the
+     * header length check will be disabled.
      * <p>
-     * A message header may be folded across multiple lines. This configuration parameter
-     * is used to limit the total length of a header, i.e. the sum of the length of all
-     * lines the header spans across (including line terminators).
+     * A message header may be folded across multiple lines. This configuration
+     * parameter is used to limit the total length of a header, i.e. the sum of
+     * the length of all lines the header spans across (including line
+     * terminators).
      * <p>
      * Default value: <code>10000</code>
      * 
-     * @param maxHeaderLen maximum header length limit
+     * @param maxHeaderLen
+     *            maximum header length limit
      */
     public void setMaxHeaderLen(int maxHeaderLen) {
         this.maxHeaderLen = maxHeaderLen;
     }
 
-    /** 
+    /**
      * Returns the maximum content length limit
+     * 
      * @see #setMaxContentLen(long)
      * 
      * @return value of the the maximum content length limit
@@ -182,14 +193,15 @@ public final class MimeEntityConfig implements Cloneable {
     }
 
     /**
-     * Sets the maximum content length limit. Parsing of a MIME entity will be terminated 
-     * with a {@link MimeException} if a content body exceeds the maximum length limit. 
-     * If this parameter is set to a non positive value the content length
-     * check will be disabled.
+     * Sets the maximum content length limit. Parsing of a MIME entity will be
+     * terminated with a {@link MimeException} if a content body exceeds the
+     * maximum length limit. If this parameter is set to a non positive value
+     * the content length check will be disabled.
      * <p>
      * Default value: <code>-1</code>
      * 
-     * @param maxContentLen maximum content length limit
+     * @param maxContentLen
+     *            maximum content length limit
      */
     public void setMaxContentLen(long maxContentLen) {
         this.maxContentLen = maxContentLen;
@@ -218,11 +230,10 @@ public final class MimeEntityConfig implements Cloneable {
     }
 
     /**
-     * Returns the value of the default content type. 
-     * When not null, indicates that the parsing should be headless.
+     * Returns the value of the default content type. When not null, indicates
+     * that the parsing should be headless.
      * 
-     * @return default content type when parsing headless,
-     * null otherwise
+     * @return default content type when parsing headless, null otherwise
      * @see org.apache.james.mime4j.parser.MimeStreamParser#parse(java.io.InputStream)
      */
     public String getHeadlessParsing() {
@@ -230,14 +241,14 @@ public final class MimeEntityConfig implements Cloneable {
     }
 
     /**
-     * Defines a default content type. 
-     * When not null, indicates that the parsing should be headless.
+     * Defines a default content type. When not null, indicates that the parsing
+     * should be headless.
      * <p>
      * Default value: <code>null</code>
      * 
      * @param contentType
-     *            value of the default content type when parsing headless,
-     *            null otherwise
+     *            value of the default content type when parsing headless, null
+     *            otherwise
      * @see org.apache.james.mime4j.parser.MimeStreamParser#parse(java.io.InputStream)
      */
     public void setHeadlessParsing(String contentType) {
@@ -253,13 +264,10 @@ public final class MimeEntityConfig implements Cloneable {
             throw new InternalError();
         }
     }
-    
+
     @Override
     public String toString() {
-        return "[strict parsing: " + strictParsing + ", max line length: "
-                + maxLineLen + ", max header count: " + maxHeaderCount
-                + ", max content length: " + maxContentLen
-                + ", count line numbers: " + countLineNumbers + "]";
+        return "[strict parsing: " + strictParsing + ", max line length: " + maxLineLen + ", max header count: " + maxHeaderCount + ", max content length: " + maxContentLen + ", count line numbers: " + countLineNumbers + "]";
     }
-    
+
 }
