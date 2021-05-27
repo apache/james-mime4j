97d276943ddadec09ea5879e2553ead7b4c546a1
Benoit Tellier <btellier@linagora.com>
2021-05-27 14:46:56 +0700

    [WARNING] Remove unecessary toString calls
    The StringBuilder handles the conversion
    itself.

--- core/src/main/java/org/apache/james/mime4j/stream/ParserCursor.java
+++ core/src/main/java/org/apache/james/mime4j/stream/ParserCursor.java
@@ -79,11 +79,11 @@ public class ParserCursor {
     public String toString() {
         StringBuilder buffer = new StringBuilder();
         buffer.append('[');
-        buffer.append(Integer.toString(this.lowerBound));
+        buffer.append(this.lowerBound);
         buffer.append('>');
-        buffer.append(Integer.toString(this.pos));
+        buffer.append(this.pos);
         buffer.append('>');
-        buffer.append(Integer.toString(this.upperBound));
+        buffer.append(this.upperBound);
         buffer.append(']');
         return buffer.toString();
     }
