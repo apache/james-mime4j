1d872609404c3dd139d443a979597ce21a09bd47
Benoit Tellier <btellier@linagora.com>
2021-06-16 13:01:48 +0700

    MIME4J-298 Convert DateTimeFieldLenientImpl to DateTimeFormatter
    This allows:
     - Specifying all patterns at once, avoiding one parsing pass per pattern
     - DateTimeFormatter is thread safe, thus can be initialized once and reused

--- dom/src/main/java/org/apache/james/mime4j/field/DateTimeFieldLenientImpl.java
+++ dom/src/main/java/org/apache/james/mime4j/field/DateTimeFieldLenientImpl.java
@@ -19,14 +19,23 @@
 
 package org.apache.james.mime4j.field;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Collections;
+import static java.time.temporal.ChronoField.DAY_OF_MONTH;
+import static java.time.temporal.ChronoField.DAY_OF_WEEK;
+import static java.time.temporal.ChronoField.HOUR_OF_DAY;
+import static java.time.temporal.ChronoField.MILLI_OF_SECOND;
+import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
+import static java.time.temporal.ChronoField.MONTH_OF_YEAR;
+import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
+import static java.time.temporal.ChronoField.YEAR;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.SignStyle;
 import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.TimeZone;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.james.mime4j.codec.DecodeMonitor;
 import org.apache.james.mime4j.dom.FieldParser;
@@ -37,34 +46,80 @@ import org.apache.james.mime4j.stream.Field;
  * Date-time field such as <code>Date</code> or <code>Resent-Date</code>.
  */
 public class DateTimeFieldLenientImpl extends AbstractField implements DateTimeField {
+    private static final int INITIAL_YEAR = 1970;
+    public static final DateTimeFormatter RFC_5322 = new DateTimeFormatterBuilder()
+        .parseCaseInsensitive()
+        .parseLenient()
+        .optionalStart()
+            .appendText(DAY_OF_WEEK, dayOfWeek())
+            .appendLiteral(", ")
+        .optionalEnd()
+        .appendValue(DAY_OF_MONTH, 1, 2, SignStyle.NOT_NEGATIVE)
+        .appendLiteral(' ')
+        .appendText(MONTH_OF_YEAR, monthOfYear())
+        .appendLiteral(' ')
+        .appendValueReduced(YEAR, 2, 4, INITIAL_YEAR)
+        .appendLiteral(' ')
+        .appendValue(HOUR_OF_DAY, 2)
+        .appendLiteral(':')
+        .appendValue(MINUTE_OF_HOUR, 2)
+        .optionalStart()
+            .appendLiteral(':')
+            .appendValue(SECOND_OF_MINUTE, 2)
+        .optionalEnd()
+        .optionalStart()
+            .appendLiteral('.')
+            .appendValue(MILLI_OF_SECOND, 3)
+        .optionalEnd()
+        .optionalStart()
+            .appendLiteral(' ')
+            .appendOffset("+HHMM", "GMT")
+        .optionalEnd()
+        .optionalStart()
+            .appendLiteral(' ')
+            .appendOffsetId()
+        .optionalEnd()
+        .optionalStart()
+            .appendLiteral(' ')
+            .appendPattern("0000")
+        .optionalEnd()
+        .toFormatter()
+        .withZone(ZoneId.of("GMT"));
 
-    private static final String[] DEFAULT_DATE_FORMATS = {
-        "EEE, dd MMM yy HH:mm:ss ZZZZ",
-        "dd MMM yy HH:mm:ss ZZZZ",
-        "EEE, dd MMM yy HH:mm:ss.SSS 0000",
-        "EEE, dd MMM yy HH:mm:ss 0000",
-        "EEE, dd MMM yyyy HH:mm:ss ZZZZ",
-        "dd MMM yyyy HH:mm:ss ZZZZ",
-        "EEE, dd MMM yyyy HH:mm:ss.SSS 0000",
-        "EEE, dd MMM yyyy HH:mm:ss 0000",
-        "EEE, dd MMM yy HH:mm:ss X",
-        "dd MMM yy HH:mm:ss X",
-        "EEE, dd MMM yy HH:mm:ss.SSS X",
-        "EEE, dd MMM yy HH:mm:ss X",
-        "EEE, dd MMM yyyy HH:mm:ss X",
-        "dd MMM yyyy HH:mm:ss X",
-        "EEE, dd MMM yyyy HH:mm:ss.SSS X",
-        "EEE, dd MMM yyyy HH:mm:ss X",
-    };
+    private static Map<Long, String> monthOfYear() {
+        HashMap<Long, String> result = new HashMap<>();
+        result.put(1L, "Jan");
+        result.put(2L, "Feb");
+        result.put(3L, "Mar");
+        result.put(4L, "Apr");
+        result.put(5L, "May");
+        result.put(6L, "Jun");
+        result.put(7L, "Jul");
+        result.put(8L, "Aug");
+        result.put(9L, "Sep");
+        result.put(10L, "Oct");
+        result.put(11L, "Nov");
+        result.put(12L, "Dec");
+        return result;
+    }
 
-    private final List<String> datePatterns;
+    private static Map<Long, String> dayOfWeek() {
+        HashMap<Long, String> result = new HashMap<>();
+        result.put(1L, "Mon");
+        result.put(2L, "Tue");
+        result.put(3L, "Wed");
+        result.put(4L, "Thu");
+        result.put(5L, "Fri");
+        result.put(6L, "Sat");
+        result.put(7L, "Sun");
+        return result;
+    }
 
     private boolean parsed = false;
     private Date date;
 
     private DateTimeFieldLenientImpl(Field rawField, DecodeMonitor monitor) {
         super(rawField, monitor);
-        this.datePatterns = Collections.unmodifiableList(Arrays.asList(DEFAULT_DATE_FORMATS));
     }
 
     public Date getDate() {
@@ -81,16 +136,7 @@ public class DateTimeFieldLenientImpl extends AbstractField implements DateTimeF
         if (body != null) {
             body = body.trim();
         }
-        for (String datePattern : datePatterns) {
-            try {
-                SimpleDateFormat parser = new SimpleDateFormat(datePattern, Locale.US);
-                parser.setTimeZone(TimeZone.getTimeZone("GMT"));
-                parser.setLenient(true);
-                date = parser.parse(body);
-                break;
-            } catch (ParseException ignore) {
-            }
-        }
+        date = Date.from(Instant.from(RFC_5322.parse(body)));
     }
 
     public static final FieldParser<DateTimeField> PARSER = new FieldParser<DateTimeField>() {
