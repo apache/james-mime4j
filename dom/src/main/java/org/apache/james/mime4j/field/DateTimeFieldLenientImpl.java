fe25dbd87dbf355c25242f24b9d5861d26095fbb
Benoit Tellier <btellier@linagora.com>
2021-06-20 10:29:17 +0700

    MIME-298 DateTimeFieldLenientImpl: remove unneeded option
    Removing it breaks no tests, as 0000 is the
    default GMT.
    
    This prevents one defensive copy of the
    parsed entity thus gets parsing slightly
    faster.

--- dom/src/main/java/org/apache/james/mime4j/field/DateTimeFieldLenientImpl.java
+++ dom/src/main/java/org/apache/james/mime4j/field/DateTimeFieldLenientImpl.java
@@ -36,8 +36,6 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.ResolverStyle;
 import java.time.format.SignStyle;
-import java.time.temporal.ChronoField;
-import java.time.temporal.TemporalField;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
@@ -84,10 +82,6 @@ public class DateTimeFieldLenientImpl extends AbstractField implements DateTimeF
         .optionalStart()
             .appendLiteral(' ')
             .appendOffsetId()
-        .optionalEnd()
-        .optionalStart()
-            .appendLiteral(' ')
-            .appendPattern("0000")
         .optionalEnd()
         .toFormatter()
         .withZone(ZoneId.of("GMT"))
