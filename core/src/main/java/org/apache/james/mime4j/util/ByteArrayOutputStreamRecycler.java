4216ca27396b3516ad874d89a60868c5032b5068
Benoit Tellier <btellier@linagora.com>
2022-08-25 08:19:57 +0700

    MIME4J-318 Allow disabling buffer recycling

--- core/src/main/java/org/apache/james/mime4j/util/ByteArrayOutputStreamRecycler.java
+++ core/src/main/java/org/apache/james/mime4j/util/ByteArrayOutputStreamRecycler.java
@@ -58,7 +58,7 @@ public class ByteArrayOutputStreamRecycler {
     }
 
     private void release(UnsynchronizedByteArrayOutputStream value) {
-        if (value != null) {
+        if (value != null && BufferRecycler.ENABLED) {
             value.reset();
             buffers.offer(value);
         }
