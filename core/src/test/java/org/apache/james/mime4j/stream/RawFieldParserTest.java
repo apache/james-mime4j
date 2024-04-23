apache/james-mime4j

commit d5ddd879a7fd4e1e1e7c9edb30850a43031fa7b3

Content-Disposition filename Q and UTF-8 encoded.

core/src/main/java/org/apache/james/mime4j/stream/RawFieldParser.java
@@ -19,11 +19,15 @@
 
 package org.apache.james.mime4j.stream;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
 
 import org.apache.james.mime4j.MimeException;
+import org.apache.james.mime4j.codec.DecodeMonitor;
+import org.apache.james.mime4j.codec.DecoderUtil;
+import org.apache.james.mime4j.util.ByteArrayBuffer;
 import org.apache.james.mime4j.util.ByteSequence;
 import org.apache.james.mime4j.util.CharsetUtil;
 import org.apache.james.mime4j.util.ContentUtil;
@@ -191,6 +195,12 @@ public class RawFieldParser {
      *  is not delimited by any character.
      */
     public String parseValue(final ByteSequence buf, final ParserCursor cursor, final BitSet delimiters) {
+        if (!CharsetUtil.isASCII(buf)) {
+            String value = parseUtf8Filename(buf);
+            if (value != null)
+                return value;
+        }
+
         StringBuilder dst = new StringBuilder();
         boolean whitespace = false;
         while (!cursor.atEnd()) {
@@ -217,6 +227,25 @@ public class RawFieldParser {
             }
         }
         return dst.toString();
+    }
+
+    /**
+     * Special case for parsing {@code filename} attribute in nonstandard encoding like:
+     * {@code Content-Disposition: attachment; filename="УПД ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ "СТАНЦИЯ ВИРТУАЛЬНАЯ" 01-05-21.pdf"}
+     * 
+     * @param buf field raw.
+     * @return filename value or {@code null}.
+     */
+    private String parseUtf8Filename(ByteSequence buf) {
+        final String value = new String(buf.toByteArray(), StandardCharsets.UTF_8);
+
+        final String prefix = "filename=\"";
+        final int pos = value.indexOf(prefix);
+        if (pos > 0) {
+            return value.substring(pos + prefix.length(), value.length() - 1);
+        }
+        
+        return null;
     }
 
     /**
@@ -384,14 +413,17 @@ public class RawFieldParser {
         }
         pos++;
         indexFrom++;
+
+        ByteArrayBuffer dstRaw = new ByteArrayBuffer(200);
+
         boolean escaped = false;
         for (int i = indexFrom; i < indexTo; i++, pos++) {
             current = (char) (buf.byteAt(i) & 0xff);
             if (escaped) {
                 if (current != '\"' && current != '\\') {
-                    dst.append('\\');
+                    dstRaw.append('\\');
                 }
-                dst.append(current);
+                dstRaw.append(current);
                 escaped = false;
             } else {
                 if (current == '\"') {
@@ -401,10 +433,18 @@ public class RawFieldParser {
                 if (current == '\\') {
                     escaped = true;
                 } else if (current != '\r' && current != '\n') {
-                    dst.append(current);
+                    dstRaw.append(current);
                 }
             }
         }
+
+        String decoded = ContentUtil.decode(dstRaw);
+        if (decoded.startsWith("=?")) {
+            decoded = DecoderUtil.decodeEncodedWords(decoded, DecodeMonitor.STRICT);
+        }
+
+        dst.append(decoded);
+
         cursor.updatePos(pos);
     }
 

core/src/main/java/org/apache/james/mime4j/util/CharsetUtil.java
@@ -57,6 +57,23 @@ public class CharsetUtil {
     }
 
     /**
+     * Returns <code>true</code> if the specified byte array consists entirely of
+     * US ASCII characters.
+     *
+     * @param raw
+     *            byte array to test.
+     * @return <code>true</code> if the specified string consists entirely of
+     *         US ASCII characters, <code>false</code> otherwise.
+     */
+    public static boolean isASCII(ByteSequence raw) {
+        for (int i = 0; i < raw.length(); i++) {
+            if (!isASCII((char) (raw.byteAt(i) & 0xff)))
+                return false;
+        }
+        return true;
+    }
+
+    /**
      * Returns <code>true</code> if the specified string consists entirely of
      * US ASCII characters.
      *

dom/src/test/java/org/apache/james/mime4j/field/LenientContentDispositionFieldTest.java
@@ -19,22 +19,29 @@
 
 package org.apache.james.mime4j.field;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
 import org.apache.james.mime4j.MimeException;
 import org.apache.james.mime4j.dom.field.ContentDispositionField;
 import org.apache.james.mime4j.stream.RawField;
 import org.apache.james.mime4j.stream.RawFieldParser;
+import org.apache.james.mime4j.util.ByteArrayBuffer;
 import org.apache.james.mime4j.util.ByteSequence;
 import org.apache.james.mime4j.util.ContentUtil;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Date;
 
 public class LenientContentDispositionFieldTest {
 
     static ContentDispositionField parse(final String s) throws MimeException {
         ByteSequence raw = ContentUtil.encode(s);
         RawField rawField = RawFieldParser.DEFAULT.parseField(raw);
+        return ContentDispositionFieldLenientImpl.PARSER.parse(rawField, null);
+    }
+
+    static ContentDispositionField parse(final byte[] raw) throws MimeException {
+        RawField rawField = RawFieldParser.DEFAULT.parseField(new ByteArrayBuffer(raw, true));
         return ContentDispositionFieldLenientImpl.PARSER.parse(rawField, null);
     }
 
@@ -109,6 +116,29 @@ public class LenientContentDispositionFieldTest {
 
         f = parse("Content-Disposition: inline");
         Assert.assertNull(f.getFilename());
+    }
+
+    @Test
+    public void testGetFilenameEncoded() throws Exception {
+        byte[] data = ("Content-Disposition: attachment;\n" +
+            " FileName=\"=?WINDOWS-1251?Q?3244659=5F=C0=EA=F2_=E7=E0_=C8=FE=EB=FC_?=\n" +
+            " =?WINDOWS-1251?Q?2020.pdf?=\"")
+            .getBytes(StandardCharsets.UTF_8);
+        
+        ContentDispositionField f = parse(data);
+
+        Assert.assertEquals("WINDOWS-1251 Q encoded filename", "3244659_Акт за Июль 2020.pdf", f.getFilename());
+    }
+
+    @Test
+    public void testGetFilenameUtf8() throws Exception {
+        byte[] data = 
+            "Content-Disposition: attachment; filename=\"УПД ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ \"СТАНЦИЯ ВИРТУАЛЬНАЯ\" 01-05-21.pdf\""
+            .getBytes(StandardCharsets.UTF_8);
+
+        ContentDispositionField f = parse(data);
+
+        Assert.assertEquals("UTF8 encoded filename", "УПД ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ \"СТАНЦИЯ ВИРТУАЛЬНАЯ\" 01-05-21.pdf", f.getFilename());
     }
 
     @Test

commit 30cf4381bacf6f36bc3fa46840e8110136e893c9

Fixes parsing headers containing UTF-8 characters. Regression caused by commit d5ddd879a7fd4e1e1e7c9edb30850a43031fa7b3.

core/src/main/java/org/apache/james/mime4j/stream/RawFieldParser.java
@@ -196,7 +196,7 @@ public class RawFieldParser {
      */
     public String parseValue(final ByteSequence buf, final ParserCursor cursor, final BitSet delimiters) {
         if (!CharsetUtil.isASCII(buf)) {
-            String value = parseUtf8Filename(buf);
+            String value = parseUtf8Filename(buf, cursor);
             if (value != null)
                 return value;
         }
@@ -236,16 +236,24 @@ public class RawFieldParser {
      * @param buf field raw.
      * @return filename value or {@code null}.
      */
-    private String parseUtf8Filename(ByteSequence buf) {
-        final String value = new String(buf.toByteArray(), StandardCharsets.UTF_8);
-
+    private String parseUtf8Filename(ByteSequence buf, final ParserCursor cursor) {
         final String prefix = "filename=\"";
-        final int pos = value.indexOf(prefix);
-        if (pos > 0) {
-            return value.substring(pos + prefix.length(), value.length() - 1);
+        if (cursor.getPos() < prefix.length()) {
+            return null;
         }
-        
-        return null;
+        //ensure filename prefix is right before the current cursor position
+        for (int pos = prefix.length(); pos >= 0; --pos) {
+            char current = (char) (buf.byteAt(cursor.getPos() - pos) & 0xff);
+            if (current != prefix.charAt(prefix.length() - pos)) {
+                return null;
+            }
+        }
+
+        byte[] bytes = new byte[cursor.getUpperBound() - cursor.getPos()];
+        for (int pos = cursor.getPos(); pos < cursor.getUpperBound(); ++pos){
+            bytes[pos - cursor.getPos()] = buf.byteAt(pos);
+        }
+        return new String(bytes, StandardCharsets.UTF_8);
     }
 
     /**

core/src/test/java/org/apache/james/mime4j/stream/RawFieldParserTest.java
@@ -461,4 +461,37 @@ public class RawFieldParserTest {
         org.junit.Assert.assertEquals("simple boundary", params.get(0).getValue());
     }
 
+    @Test
+    public void testRegressionForContentDispositionParsingASCIIonly() {
+        ByteSequence buf = ContentUtil.encode(
+                "Content-Disposition: form-data; name=\"filedata\"; filename=\"Sanity a.doc\"");
+        ParserCursor cursor = new ParserCursor(0, buf.length());
+        List<NameValuePair> params = parser.parseParameters(buf, cursor);
+
+        org.junit.Assert.assertEquals(3, params.size());
+        org.junit.Assert.assertEquals("Content-Disposition: form-data", params.get(0).getName());
+        org.junit.Assert.assertEquals(null, params.get(0).getValue());
+        org.junit.Assert.assertEquals("name", params.get(1).getName());
+        org.junit.Assert.assertEquals("filedata", params.get(1).getValue());
+        org.junit.Assert.assertEquals("filename", params.get(2).getName());
+        org.junit.Assert.assertEquals("Sanity a.doc", params.get(2).getValue());
+   }
+
+    @Test
+    public void testRegressionForContentDispositionParsingUTF8() {
+        ByteSequence buf = ContentUtil.encode(
+                "Content-Disposition: form-data; name=\"filedata\"; filename=\"Sanity ä.doc\"");
+        ParserCursor cursor = new ParserCursor(0, buf.length());
+        List<NameValuePair> params = parser.parseParameters(buf, cursor);
+
+        org.junit.Assert.assertEquals(3, params.size());
+        org.junit.Assert.assertEquals("Content-Disposition: form-data", params.get(0).getName());
+        org.junit.Assert.assertEquals(null, params.get(0).getValue());
+        org.junit.Assert.assertEquals("name", params.get(1).getName());
+        org.junit.Assert.assertEquals("filedata", params.get(1).getValue());
+        org.junit.Assert.assertEquals("filename", params.get(2).getName());
+        org.junit.Assert.assertEquals("Sanity ä.doc", params.get(2).getValue());
+   }
+
+
 }