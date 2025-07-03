d43feae84ffe6d484ada025df69b8ba9ef984451
tallison <tallison@apache.org>
2025-07-03 11:37:44 -0400

    MIME4J-332 -- clear up extended vs continuation handling

--- core/src/main/java/org/apache/james/mime4j/util/MimeParameterMapping.java
+++ core/src/main/java/org/apache/james/mime4j/util/MimeParameterMapping.java
@@ -19,71 +19,155 @@
 
 package org.apache.james.mime4j.util;
 
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.james.mime4j.MimeException;
+
+/**
+ * This class tracks parameter mappings and tries to respect <a href="https://www.rfc-editor.org/rfc/rfc2231">rfc2231</a>
+ * and conform roughly to Thunderbird's behavior as described on <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=588781>bug 588781</a>.
+ * <p>
+ * See <a href="https://issues.apache.org/jira/browse/MIME4J-332">MIME4J-332</a>
+ * <p>
+ *     These are the behaviors:
+ * <ul>
+ *    <li>If there are multiple keys for standard parameters, use the first and ignore the rest</li>
+ *    <li>If there are multiple keys for extended parameters, use the first and ignore the rest</li>
+ *    <li>If there's an extended and standard key for a given field, prefer the extended</li>
+ *    <li>If there's an extended and a continuation, prefer the extended, recognizing that a continuation field may also include charset information</li>
+ * </ul>
+ */
 public class MimeParameterMapping {
 
-    private final Map<String, String> parameters = new HashMap<>();
-    private final Set<String> hasSection = new HashSet<>();
+    /**
+     * What type of parameter name is it
+     */
+    enum PARAMETER_TYPE {
+        /**
+         * standard parameter name, e.g. "filename"
+         */
+        STANDARD,
+        /**
+         * extended parameter without continuation numbers, e.g. "filename*", typically a sign that charset is included
+         */
+        EXTENDED,
+        /**
+         * continuation parameter, e.g. "filename*0", used to extend field lengths beyond the initial length limit, may also
+         * include charset information like {@link PARAMETER_TYPE#EXTENDED}
+         */
+        CONTINUATION
+    }
 
-    /** Charset, taken from the first item added to {@link #parameters}. */
+    //this could be more precise and require only a *\\d+\\Z
+    private static final Pattern STAR_AND_NUMBER = Pattern.compile("\\*\\d");
+
+    private final Set<String> parameterNames = new HashSet<>();
+    private final Map<String, String> standard = new HashMap<>();
+    private final Map<String, String> extended = new HashMap<>();
+    private final Map<String, String> continuation = new HashMap<>();
+
+    private final Map<String, String> parameters = new HashMap<>();
+    private boolean needToUpdate = true;
+
+    /** Charset, taken from the first item added via {@link #addParameter(String, String)}. */
     private String charset;
 
     public Map<String, String> getParameters() {
-        return parameters;
+        if (needToUpdate) {
+            updateParameters();
+        }
+        return Collections.unmodifiableMap(parameters);
     }
 
+    private void updateParameters() {
+        parameters.clear();
+        for (String param : parameterNames) {
+            parameters.put(param, get(param));
+        }
+        needToUpdate = false;
+    }
+
+    /**
+     * Applies the algorithm described in the class javadoc and returns
+     * the decoded value from the best option.
+     *
+     * @param name field name (must be lowercased
+     * @return
+     */
     public String get(String name) {
-        return parameters.get(name);
+        if (! parameterNames.contains(name)) {
+            return null;
+        }
+
+        if (extended.containsKey(name)) {
+            try {
+                return decodeParameterValue(extended.get(name));
+            } catch (DecodeException e) {
+                //ignore and try next
+            }
+        }
+        if (continuation.containsKey(name)) {
+            try {
+                return decodeParameterValue(continuation.get(name));
+            } catch (DecodeException e) {
+                //ignore and try standard
+            }
+        }
+        if (standard.containsKey(name)) {
+            try {
+                return decodeParameterValue(standard.get(name));
+            } catch (DecodeException e) {
+                //ignore and try without decoding
+            }
+        }
+        //couldn't decode anything. not clear what the "spec" says is the right behavior
+        //For now, back off in reverse order
+        if (standard.containsKey(name)) {
+            return standard.get(name);
+        }
+
+        if (continuation.containsKey(name)) {
+            return continuation.get(name);
+        }
+
+        if (extended.containsKey(name)) {
+            return extended.get(name);
+        }
+        //this should be unreachable
+        return null;
     }
 
     public void addParameter(final String name, String value) {
-        String key = name;
-        int sectionDelimiter = name.indexOf("*");
-        if (sectionDelimiter > -1) {
-            key = key.substring(0, sectionDelimiter);
-        }
-        key = key.toLowerCase(Locale.ROOT);
-        //We're conforming to Thunderbird's behavior: https://bugzilla.mozilla.org/show_bug.cgi?id=588781
-        //See MIME4J-332
-        //If there are sectioned and non-sectioned keys for a given field, prefer the sectioned keys
-        //If there are multiple non-sectioned keys, use the first and ignore the rest
-
-        //switch on whether there's a section in the key
-        if (sectionDelimiter < 0) {
-            handleNoSection(key, value);
-        } else {
-            handleSection(key, value);
+        needToUpdate = true;
+        ParameterTypePair parameterTypePair = getParameterTypePair(name);
+        parameterNames.add(parameterTypePair.fieldName);
+        switch (parameterTypePair.fieldType) {
+            case EXTENDED:
+                extended.putIfAbsent(parameterTypePair.fieldName, value);
+                break;
+            case CONTINUATION:
+                if (continuation.containsKey(parameterTypePair.fieldName)) {
+                    String newValue = continuation.get(parameterTypePair.fieldName) + value;
+                    continuation.put(parameterTypePair.fieldName, newValue);
+                } else {
+                    continuation.put(parameterTypePair.fieldName, value);
+                }
+                break;
+            case STANDARD:
+                standard.putIfAbsent(parameterTypePair.fieldName, value);
         }
     }
 
-    private void handleSection(String key, String value) {
-        //if the key/value we already stored in parameters was not sectioned, remove it.
-        if (! hasSection.contains(key)) {
-            parameters.remove(key);
-        }
-        if (parameters.containsKey(key)) {
-            parameters.put(key, decodeParameterValue(parameters.get(key) + value));
-        } else {
-            parameters.put(key, decodeParameterValue(value));
-        }
-        hasSection.add(key);
-    }
-
-    private void handleNoSection(String key, String value) {
-        if (parameters.containsKey(key)) {
-            //if there's already a value here, and this is a no section key
-            //ignore this value
-            return;
-        }
-        parameters.put(key, decodeParameterValue(value));
-    }
-
-    private String decodeParameterValue(String value) {
+    private String decodeParameterValue(String value) throws DecodeException {
         if (value == null) {
             return null;
         }
@@ -98,20 +182,65 @@ public class MimeParameterMapping {
         }
         charset = value.substring(0, charsetEnd);
         String fileName = value.substring(languageEnd + 1);
+        //check that the charset is valid
+        try {
+            Charset.forName(charset);
+        } catch (IllegalArgumentException e) {
+            return fileName;
+        }
         return urlDecode(fileName);
     }
 
-    private String urlDecode(String value) {
+    private String urlDecode(String value) throws DecodeException {
         try {
             return java.net.URLDecoder.decode(value, charset);
-        }
-        catch (Exception ignore) {
-            return value;
+        } catch (Exception e) {
+            throw new DecodeException(e);
         }
     }
 
-    private String removeSectionFromName(String parameterName) {
+    private ParameterTypePair getParameterTypePair(String parameterName) {
         int position = parameterName.indexOf('*');
-        return parameterName.substring(0, position < 0 ? parameterName.length() : position);
+        if (position < 0) {
+            return new ParameterTypePair(parameterName.toLowerCase(Locale.ROOT), PARAMETER_TYPE.STANDARD);
+        }
+        String fieldName = parameterName.substring(0, position);
+        fieldName = fieldName.toLowerCase(Locale.ROOT);
+
+        String starAndAfter = parameterName.substring(position);
+        if ("*".equals(starAndAfter)) {
+            return new ParameterTypePair(fieldName, PARAMETER_TYPE.EXTENDED);
+        }
+        Matcher m = STAR_AND_NUMBER.matcher(starAndAfter);
+        if (m.find()) {
+            return new ParameterTypePair(fieldName, PARAMETER_TYPE.CONTINUATION);
+        }
+        //something is weird, treat this as a STAR?
+        return new ParameterTypePair(fieldName, PARAMETER_TYPE.EXTENDED);
+    }
+
+    private static class ParameterTypePair {
+        private final String fieldName;
+        private final PARAMETER_TYPE fieldType;
+
+        public ParameterTypePair(String fieldName, PARAMETER_TYPE fieldType) {
+            this.fieldName = fieldName;
+            this.fieldType = fieldType;
+        }
+    }
+
+    private static class DecodeException extends MimeException {
+
+        public DecodeException(String message) {
+            super(message);
+        }
+
+        public DecodeException(Throwable cause) {
+            super(cause);
+        }
+
+        public DecodeException(String message, Throwable cause) {
+            super(message, cause);
+        }
     }
 }
